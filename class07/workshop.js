c7c95dc55c9a0e77b25ca4ce38c535f7fb0bba11
Jamoliddin Muminov <jmuminov92@gmail.com>
2024-10-28 20:22:53 -0400

    Added pseudocode as a comment to the .js file

--- class07/workshop.js
+++ class07/workshop.js
@@ -1,3 +1,9 @@
+/*
+1. First I will create three const valirables and name them numOne, numTwo, numThree
+2. I will assign numbers to them per the assignment using the ariphmetic arguments
+3. Next, I will create const variable where I would concatinate all three strings together
+4. Lastly, I will use alert() method to display the concatinated strings under variable named combo
+*/
 const numOne = 100 / 10;
 const numTwo = 5 * 8;
 const numThree = 40 - 1;
